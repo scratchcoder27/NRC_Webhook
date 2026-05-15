908f8f59ef51b69b0b63a98ad48ff0079438b56f
ScratchCoder027 <debarpanmukherjee27@gmail.com>
2026-05-15 20:00:00 +0530

    More descriptive var names, and type hints

---
+++ src/reports_main.py
@@ -0,0 +1,135 @@
+import requests
+from bs4 import BeautifulSoup
+import discord
+import json
+from time import sleep
+from os import getenv
+from dotenv import load_dotenv
+
+# MARK: CONFIG
+url_test="https://web.archive.org/web/20251215120022/https://www.nrc.gov/reading-rm/doc-collections/event-status/event/en"
+url_test2="https://web.archive.org/web/20231118072408/https://www.nrc.gov/reading-rm/doc-collections/event-status/event/en"
+url = "https://www.nrc.gov/reading-rm/doc-collections/event-status/event/en.html"
+
+load_dotenv()
+
+WEBHOOK_URL_REPORT=getenv("WEBHOOK_URL_REPORT")
+BUFFER_SIZE = 1950 # discord has 2000 limit
+JSON_FILE_PATH = "src/facility.json"
+SLEEP_TIME = 10 # secs prev: 600
+DEBUG = True
+
+# MARK: HELPERS
+def replace_text(obj, old, new):
+    if isinstance(obj, str):
+        # Convert special Discord objects to strings automatically
+        if isinstance(new, discord.Role):
+            new = new.mention
+        elif isinstance(new, discord.Member):
+            new = new.mention
+        else:
+            new = str(new)
+        return obj.replace(old, new)
+    elif isinstance(obj, list):
+        return [replace_text(item, old, new) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: replace_text(value, old, new) for key, value in obj.items()}
+    return obj
+
+# MARK: GET DATA
+try:
+    response = requests.get(url_test)
+except Exception as e:
+    print("Error while getting data: " + e)
+
+if response.status_code != 200:
+    print("Error while getting data, recieved status code " + response.status_code)
+
+# MARK: PARSING
+soup = BeautifulSoup(response.text, "lxml")
+all_event = soup.find("div", class_="event-summary number text-center") 
+raw_doc_numbers = [anchor.text for anchor in all_event]
+doc_numbers=[numbers for numbers in raw_doc_numbers if numbers.isdigit()]
+print(doc_numbers)
+
+cycle = 0
+for number in doc_numbers:
+    event_data : dict = dict()
+    print("Processing event no: " + number)
+
+    processing_event = soup.find("div", id=f"en{doc_numbers[cycle]}", class_="grid border") 
+    
+    for field in processing_event.find_all("b"):
+        key = field.get_text(strip=True).replace(":", "")
+        value = field.next_sibling
+        if isinstance(value, str):
+            value = value.strip()
+        else:
+            value = ""
+        event_data[key] = value
+    
+    text = soup.select("div.border")
+    #NRC responds with Report text adn HEADER, this filters the text out
+    odd_text = text[1::2]
+
+    # print(event_data)
+    # print(odd_text)
+    
+    with open(JSON_FILE_PATH, "r") as f:
+        facility = json.load(f) # schema
+        # Replace text
+    
+    # MARK: INTERPRET DATA
+        facility : dict = replace_text(facility, "numberssss", doc_numbers[cycle])
+
+
+    # with open("test.json", "w") as f:
+    #     json.dump(event_data, f) 
+    # exit()
+
+
+    embed = facility
+    # MARK: DISCORD WEBHOOK
+    try:
+        
+        response = requests.post(WEBHOOK_URL_REPORT, json=embed)
+
+        if response.status_code == 204:
+            print("Message sent successfully.")
+        else:
+            print(f"Failed: {response.status_code}")
+            print(response.text)
+
+    except Exception as e:
+        print(f"Error sending header message: {e}")
+
+
+    buffer = odd_text[cycle]
+    try:
+        for chunk in buffer:
+            payload = {
+                "content": chunk.get_text("\n", strip=True)
+            }
+
+            response = requests.post(WEBHOOK_URL_REPORT, json=payload)
+
+            if response.status_code == 204:
+                print("Message sent successfully.")
+            else:
+                print(f"Failed: {response.status_code}")
+                print(response.text)
+
+            sleep(SLEEP_TIME)
+    except Exception as e:
+        print(f"Error sending text message: {e}")
+
+
+# MARK: DEBUG
+if not DEBUG:    exit()
+
+with open("nrc_events.txt", "w", encoding="utf-8") as f:
+    for i, event in enumerate(odd_text, start=1):
+        odd_text = event.get_text("\n", strip=True)
+
+        f.write(f"===== EVENT {i} =====\n")
+        f.write(odd_text + "\n\n")
