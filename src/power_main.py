7b87f7b8cfbb59d126cc31f499d0a9292e59837c
ScratchCoder027 <debarpanmukherjee27@gmail.com>
2026-05-22 17:05:18 +0530

    Breaks on some systens

--- src/power_main.py
+++ src/power_main.py
@@ -86,7 +86,7 @@ def fetch_data():
 
     print("Data fetched successfully.")
 
-    return sha256(data=response.text.encode(), usedforsecurity=False).hexdigest()
+    return sha256(response.text.encode(), usedforsecurity=False).hexdigest() # using data=... breaks on some systens
 
 
 # MARK: PARSING
